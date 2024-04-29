1e57b5ed6f9179200df5735288a0053ae9a0dd4a
Douglas Lemos <douglasm.lemos0507@gmail.com>
2024-04-29 20:58:01 -0300

    aplicando POO + vetores

---
+++ Programandonovamente/src/Entidades/metodosVetores.java
@@ -0,0 +1,16 @@
+package Entidades;
+
+public class metodosVetores {
+	private int quantidade_Numeros;
+	
+	//encapsulamentos
+	public int getQuantidade_Numeros() {
+		return quantidade_Numeros;
+	}
+	public void setQuantidade_Numeros(int number) {
+		this.quantidade_Numeros = number;
+	}
+	
+	
+	
+}
